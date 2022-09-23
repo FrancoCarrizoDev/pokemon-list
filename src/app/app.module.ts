57385e0df1ff13c7f9e0a920bb4286c00011243e
Franco Carrizo <franco.carrizo@bitlogic.io>
2022-09-23 20:03:23 -0300

    subiendo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
+
+import { LOCALE_ID } from '@angular/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, NavbarComponent, ContentComponent],
+  imports: [BrowserModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-AR',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
